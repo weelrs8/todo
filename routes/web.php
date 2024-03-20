36a3e4aaf656bb5f94f7519afd16d2587cf698ee
Wellington R. <weelrs8@gmail.com>
2024-03-20 15:51:11 -0400

    added index and platform controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PlatformController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// -----------------------------------------------------------------------------
+// Landing Page
+// -----------------------------------------------------------------------------
+Route::get('/', IndexController::class)->middleware('guest')->name('index');
+
+// -----------------------------------------------------------------------------
+// Google authentication routes
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Authenticated Routes
+// -----------------------------------------------------------------------------
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [PlatformController::class, 'dashboard'])->name('dashboard');
 });
