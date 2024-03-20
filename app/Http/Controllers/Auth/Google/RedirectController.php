af61a908986d0f64c4860332acc9d95ff3a0318a
Wellington R. <weelrs8@gmail.com>
2024-03-20 17:12:28 -0400

    redirect to google auth

--- app/Http/Controllers/Auth/Google/RedirectController.php
+++ app/Http/Controllers/Auth/Google/RedirectController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers\Auth\Google;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class RedirectController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        //
+        return Socialite::driver('google')->redirect();
     }
 }
